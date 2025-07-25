c6b1d772f15819914ed00ded799cf1c89647ac76
shreejata23guin <shreejata506@gmail.com>
2025-07-26 00:50:09 +0530

    updated commit

--- src/print/Print.java
+++ src/print/Print.java
@@ -3,6 +3,6 @@ package print;
 public class Print {
     public static void main(String[] args) {
 
-        System.out.println("I am Pookie");
+        System.out.println("I am Pookie Shreejata");
     }
 }
